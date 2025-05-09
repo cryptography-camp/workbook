7db9c4a57768cfa104259521c24eb196e84e9f62
Tim Ruffing <me@real-or-random.org>
2025-05-09 09:47:17 +0200

    biber-wrapper.sh: Add it to handle crypto.bib
    This removes the conversion logic from the update-cryptobib.sh script.
    It worked in principle, but it turned out to be not great for multiple
    reasons:
    
     - The XML format is called experimental in the docs, and this is
       probably for a reason. For example, we want a source map that
       converts the "howpublished" entries for ePrint to the proper BibLaTeX
       `eprint`/`eprinttype` fields. But these fields simply don't work in
       XML: https://github.com/plk/biblatex/issues/1428
     - biber still needs ~4s to process the XML file on my laptop (much
       better than ~90s for the bib file... But because it typically runs
       twice for our documents (due to
       https://github.com/plk/biblatex/issues/1429), this is still slowish
       and just about acceptable.
     - While the conversion from bib to XML only needs to be done rarely, it
       takes forever (~30min for me). The resulting XML file is huge, namely
       120 MB. This means we can't add it in a git repo on GitHub (the limit
       is 100 MB). Anyway, big files are a bad idea in git, and git lfs is a
       solution, but this adds yet another piece of complexity (and burden
       for coauthors).
     - Editors/IDEs/LSP servers don't understand the XML format, and so you
       don't have nice tooling. I guess some hacks are possible by obscuring
       the syntax to trick the tools into believing we use the bib file, for
       example:
           \let\myabr\addbibresource\myabr{crypto.bltxml}\iffalse\addbibresource{crypto.bib}\fi
       But that's ugly and literally obscures the code.

---
+++ biber-wrapper.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+# bib: The path to original BibTeX file (may not contain "<", ">", and "&").
+bib="vendor/cryptobib/crypto.bib"
+
+
+lastarg="${*: -1}"
+bcf_stem="${lastarg/%.bcf/}"
+bcf="$bcf_stem.bcf"
+bcf_dir=$(dirname "$bcf")
+jobname=$(basename "$lastarg" ".bcf")
+bib_ex="${bcf_stem}.exttmp.bib"
+
+# Switch to the directory of this script
+cd "${BASH_SOURCE%/*}" || exit 1
+
+# Do the extraction
+./vendor/extract_from_bibliography/extract_from_bibliography.py "$bcf" "$bib" > "$bib_ex"
+
+# Modify the BCF file to use the extracted BibTeX file
+bcf_ex="${bcf_stem}.exttmp.bcf"
+sed -e "s&>$bib</bcf:datasource>&>$bib_ex</bcf:datasource>&" "$bcf" > "$bcf_ex"
+
+# Call biber with the modified BCF file
+biber "${@:1:$#-1}" "$bcf_ex"
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit $ret
+fi
+
+# Rename the generated files to have the right jobname
+mv "${bcf_stem}.exttmp.bbl" "$bcf_dir/$jobname.bbl"
+mv "${bcf_stem}.exttmp.blg" "$bcf_dir/$jobname.blg"
+
+# Remove the temporary files
+#
+# We delibaretly them when an error occurs, so they can be inspected.
+rm -f "./$bcf_ex"
+rm -f "./$bib_ex"
--- latexmkrc
+++ latexmkrc
@@ -1,7 +1,12 @@
 $pdf_mode = 1;
 $pdflatex = 'pdflatex -interaction=nonstopmode -synctex=1 %O %S';
 @default_files = ('main.tex');
-$bibtex_use = 2;
 $aux_dir = 'latex.out';
 $do_cd = 1;
-$clean_ext = 'synctex.gz synctex.gz(busy) run.xml bbl bcf fdb_latexmk run tdo %R-blx.bib sil';
+
+$biber = './biber-wrapper.sh %O %S';
+$bibtex_use = 2;
+
+# sil: "silence" package
+# exttmp.*: biber-wrapper.sh
+$clean_ext = 'synctex.gz synctex.gz(busy) run.xml bbl bcf fdb_latexmk run tdo %R-blx.bib sil exttmp.bib exttmp.bcf exttmp.bbl exttmp.blg';
